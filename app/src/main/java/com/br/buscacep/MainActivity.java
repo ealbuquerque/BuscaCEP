e165135d5eaad797072df9c2ace016992aa1c7fb
Ezequias Albuquerque <ezequias.albuquerque@outlook.com.br>
2017-05-31 23:24:13 -0300

    tela home / aplicando máscara no campo CEP

--- app/src/main/java/com/br/buscacep/MainActivity.java
+++ app/src/main/java/com/br/buscacep/MainActivity.java
@@ -1,13 +1,20 @@
 package com.br.buscacep;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import com.br.buscacep.util.CustomMasks;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText textCEP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textCEP = (EditText) findViewById(R.id.editTextCEP);
+        textCEP.addTextChangedListener(CustomMasks.insert(CustomMasks.CEP_MASK, textCEP));
     }
 }
