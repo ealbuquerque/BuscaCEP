c94a58dcd6316d8c9c6ce183dbb1c093d17e62bd
Ezequias Albuquerque <ezequias.albuquerque@outlook.com.br>
2017-06-13 19:59:42 -0300

    ajuste telas (java)

--- app/src/main/java/com/br/buscacep/MainActivity.java
+++ app/src/main/java/com/br/buscacep/MainActivity.java
@@ -2,12 +2,15 @@ package com.br.buscacep;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.br.buscacep.util.CustomMasks;
 
 public class MainActivity extends AppCompatActivity {
     private EditText textCEP;
+    private Button buttonSearch;
+    private Button buttonHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         textCEP = (EditText) findViewById(R.id.editTextCEP);
         textCEP.addTextChangedListener(CustomMasks.insert(CustomMasks.CEP_MASK, textCEP));
+
+        buttonSearch = (Button) findViewById(R.id.buttonSearch);
+        buttonHistory = (Button) findViewById(R.id.buttonHistory);
     }
 }
--- app/src/main/java/com/br/buscacep/SearchCepActivity.java
+++ app/src/main/java/com/br/buscacep/SearchCepActivity.java
@@ -1,5 +1,6 @@
 package com.br.buscacep;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +8,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SearchCepActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_cep_activity);
+    }
 }
