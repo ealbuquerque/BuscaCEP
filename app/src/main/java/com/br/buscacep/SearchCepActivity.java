3d16472126da493d794cf4e4d8e4cfb65f190c17
Maurício <mauriciobaccin@gmail.com>
2017-06-20 20:06:27 -0300

    codigo inicial para onclick do pesquisar cep e request.
    seguindo doc android.

--- app/src/main/java/com/br/buscacep/SearchCepActivity.java
+++ app/src/main/java/com/br/buscacep/SearchCepActivity.java
@@ -11,6 +11,40 @@ public class SearchCepActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.search_cep_activity);
+
+        //https://developer.android.com/reference/android/widget/Button.html
+        final Button button = findViewById(R.id.buttonSearch);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
+
+    //https://developer.android.com/training/volley/simple.html#simple
+    protected void searchApiCep(){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
 }
