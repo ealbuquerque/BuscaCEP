7049cb743b0fe74442cac24182e698a409547df5
Maurício <mauricio.baccin@hotmail.com>
2017-07-04 18:34:30 -0300

    select dos list

--- app/src/main/java/com/br/buscacep/HistoryActivity.java
+++ app/src/main/java/com/br/buscacep/HistoryActivity.java
@@ -3,13 +3,18 @@ package com.br.buscacep;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.br.buscacep.db.AddressDAO;
 import com.br.buscacep.model.Address;
+import com.br.buscacep.util.ListViewAdapter;
 
 import java.util.List;
 
@@ -20,23 +25,83 @@ import java.util.List;
 public class HistoryActivity extends AppCompatActivity {
     ListView listView;
     List<Address> addresses;
-    ArrayAdapter<Address> adapter;
+    ListViewAdapter listviewadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_activity);
+        addresses = AddressDAO.getAllAddresses();
 
         listView = (ListView) findViewById(R.id.listView);
         addresses = AddressDAO.getAllAddresses();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, addresses);
-        listView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+        listviewadapter =  new ListViewAdapter(this, android.R.layout.simple_list_item_1, addresses);
+        listView.setAdapter(listviewadapter);
+        listviewadapter.notifyDataSetChanged();
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 goSearchCepResult((Address) adapterView.getItemAtPosition(i));
+            }
+        });
+
+        // Binds the Adapter to the ListView
+        listView.setAdapter(listviewadapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        // Capture ListView item click
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode,
+                                                  int position, long id, boolean checked) {
+                // Capture total checked items
+                final int checkedCount = listView.getCheckedItemCount();
+                // Set the CAB title according to total checked items
+                mode.setTitle(checkedCount + " Selected");
+                // Calls toggleSelection method from ListViewAdapter Class
+                listviewadapter.toggleSelection(position);
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete:
+                        // Calls getSelectedIds method from ListViewAdapter Class
+                        SparseBooleanArray selected = listviewadapter
+                                .getSelectedIds();
+                        // Captures all selected ids with a loop
+                        for (int i = (selected.size() - 1); i >= 0; i--) {
+                            if (selected.valueAt(i)) {
+                                Address selecteditem = listviewadapter
+                                        .getItem(selected.keyAt(i));
+                                // Remove selected items following the ids
+                                listviewadapter.remove(selecteditem);
+                            }
+                        }
+                        // Close CAB
+                        mode.finish();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                mode.getMenuInflater().inflate(R.menu.activity_main, menu);
+                return true;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                // TODO Auto-generated method stub
+                listviewadapter.removeSelection();
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                // TODO Auto-generated method stub
+                return false;
             }
         });
     }
@@ -45,9 +110,9 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         addresses = AddressDAO.getAllAddresses();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, addresses);
-        listView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+//        listviewadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, addresses);
+//        listView.setAdapter(listviewadapter);
+//        listviewadapter.notifyDataSetChanged();
     }
 
     private void goSearchCepResult(Address address) {
---
+++ app/src/main/java/com/br/buscacep/util/ListViewAdapter.java
@@ -0,0 +1,65 @@
+package com.br.buscacep.util;
+
+import android.content.Context;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import com.br.buscacep.model.Address;
+
+import java.util.List;
+
+/**
+ * Created by Maurício on 04/07/2017.
+ */
+
+public class ListViewAdapter extends ArrayAdapter<Address> {
+    Context context;
+    LayoutInflater inflater;
+    List<Address> addressList;
+    private SparseBooleanArray mSelectedItemsIds;
+
+    public ListViewAdapter(Context context, int resourceId,
+                           List<Address> addressList) {
+        super(context, resourceId, addressList);
+        mSelectedItemsIds = new SparseBooleanArray();
+        this.context = context;
+        this.addressList = addressList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void remove(Address object) {
+        addressList.remove(object);
+        notifyDataSetChanged();
+    }
+
+    public List<Address> getWorldPopulation() {
+        return addressList;
+    }
+
+    public void toggleSelection(int position) {
+        selectView(position, !mSelectedItemsIds.get(position));
+    }
+
+    public void removeSelection() {
+        mSelectedItemsIds = new SparseBooleanArray();
+        notifyDataSetChanged();
+    }
+
+    public void selectView(int position, boolean value) {
+        if (value)
+            mSelectedItemsIds.put(position, value);
+        else
+            mSelectedItemsIds.delete(position);
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedCount() {
+        return mSelectedItemsIds.size();
+    }
+
+    public SparseBooleanArray getSelectedIds() {
+        return mSelectedItemsIds;
+    }
+}
